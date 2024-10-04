fdacd483be326114deb443f87d1de09f669c3a91
Vlad <xstep759@gmail.com>
2024-10-04 21:11:41 +0900

    15

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div>
       <header className=" h-16 flex items-center justify-center mb-6 ">
         <div className="flex items-center">
-          <img className="h-10 w-10 mr-2" src="./src/assets/battery.svg" alt="" />
+          <img className="h-10 w-10 mr-2" src="../src/assets/battery.svg" alt="" />
           <h1 className="text-3xl font-bold text-black">КАТАЛОГ</h1>
         </div>
       </header>
